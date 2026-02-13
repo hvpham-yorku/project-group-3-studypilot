39d9ee0bde1037f75ddcf4c307fe8dd5c350a699
crookedg <crookedgabby@gmail.com>
2026-02-13 18:39:48 -0500

    Restore SettingsControllerTests class and methods
    with testing

--- studypilot/src/test/java/com/studypilot/studypilot/SettingsControllerTests.java
+++ studypilot/src/test/java/com/studypilot/studypilot/SettingsControllerTests.java
@@ -1,36 +1,37 @@
-# package com.studypilot.studypilot;
-# 
-# import org.junit.jupiter.api.Test;
-# import org.springframework.beans.factory.annotation.Autowired;
-# import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-# import org.springframework.boot.test.context.SpringBootTest;
-# import org.springframework.test.web.servlet.MockMvc;
-# 
-# import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-# import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-# 
-# @SpringBootTest
-# @AutoConfigureMockMvc
-# class SettingsControllerTests {
-# 
-#     @Autowired
-#     private MockMvc mockMvc;
-# 
-#     @Test
-#     void redirectsToLoginWhenNoRoleInSession() throws Exception {
-#         mockMvc.perform(get("/settings"))
-#                 .andExpect(status().is3xxRedirection())
-#                 .andExpect(redirectedUrl("/login"));
-#     }
-# 
-#     @Test
-#     void showsSettingsPageWhenRolePresent() throws Exception {
-#         mockMvc.perform(get("/settings")
-#                         .sessionAttr("role", "user")
-#                         .sessionAttr("fullName", "Test User"))
-#                 .andExpect(status().isOk())
-#                 .andExpect(view().name("settings"))
-#                 .andExpect(model().attribute("fullName", "Test User"));
-#     }
-# }
-# ------------------------------------------------
+package com.studypilot.studypilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SettingsControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void redirectsToLoginWhenNoRoleInSession() throws Exception {
+        mockMvc.perform(get("/settings"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+    }
+
+    @Test
+    void showsSettingsPageWhenRolePresent() throws Exception {
+        mockMvc.perform(get("/settings")
+                        .sessionAttr("role", "user")
+                        .sessionAttr("fullName", "Test User"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("settings"))
+                .andExpect(model().attribute("fullName", "Test User"));
+    }
+}
+
+
