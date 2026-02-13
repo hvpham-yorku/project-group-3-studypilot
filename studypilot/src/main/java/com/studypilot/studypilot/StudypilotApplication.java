89246eb419efc56e9ec09def64e969c4bc143a8c
monabbirbhuiyan <monabbir.179@gmail.com>
2026-02-12 23:36:27 -0500

    1st commit

---
+++ studypilot/src/main/java/com/studypilot/studypilot/StudypilotApplication.java
@@ -0,0 +1,13 @@
+package com.studypilot.studypilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudypilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudypilotApplication.class, args);
+	}
+
+}
---
+++ studypilot/src/test/java/com/studypilot/studypilot/StudypilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studypilot.studypilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudypilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
